071b5949ad310f5347aa977432fd9d794b83c095
Eric Huang <huangeric.j@gmail.com>
2023-09-01 21:47:36 -0700

    Import bird model

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class Bird: MonoBehaviour {
     [SerializeField] private float jumpForce = 9f;
     [SerializeField] private float gravity = 32f;
+    [SerializeField] private float rotationSpeed = 5f;
+    [SerializeField] private float smoothness = 3f;
     [SerializeField] private Rigidbody rigidBody;
     [SerializeField] private GameManager gameManager;
 
@@ -16,6 +18,10 @@ public class Bird: MonoBehaviour {
         if (Input.GetKeyDown(KeyCode.Space) && isAlive) {
             rigidBody.velocity = Vector3.up * jumpForce;
         }
+
+        Quaternion target = Quaternion.Euler(0f, 0f, rigidBody.velocity.y * rotationSpeed);
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, target, smoothness * Time.deltaTime);
     }
 
     private void OnCollisionEnter(Collision collision) {
